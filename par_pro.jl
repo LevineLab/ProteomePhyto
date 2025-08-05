b4dd25723dd0783a4b5d0335a31a94e72e234ee9
suzanaleles <58187017+suzanaleles@users.noreply.github.com>
2025-08-05 14:59:50 -0500

    Update and rename par.jl to par_pro.jl

--- par_pro.jl
+++ par_pro.jl
@@ -2,26 +2,28 @@
 # This file contains the parameter values to run the proteome model (eqn.jl) #
 #----------------------------------------------------------------------------#
 
-# January 2024; by Suzana Goncalves Leles
+# July 28, 2025; by Suzana Goncalves Leles
+
+# Parameters to simulate "Prochlorococcus".
 
 # Maximum turnover rates for all reactions (# molecules per protein per minute)
 kref = Dict("tr" => 120.0, "ri" => 114.0, "lb" => 120.0, "p" => 7800.0,
-            "ru" => 157.0, "ld" => 120.0, "gl" => 120.0, "re" => 1000.0, "d" => 500.0)
+            "ru" => 157.0, "ld" => 120.0, "gl" => 120.0, "re" => 1000.0, "d" => 1000.0)
 
 # Half-saturation constants for all reactions
-K = Dict("tr" => 2.0, "ri" => 10^4, "lb" => 10^4, "p" => 1,
+K = Dict("tr" => 0.1, "ri" => 10^4, "lb" => 10^4, "p" => 50,
           "ru" => 1.0, "ld" => 10^4, "gl" => 10^4, "re" => 10^4, "d" => 10^4)
 
 # Space and density constraints
 s_tr = 1.26e-5                   # um2, specific surface area of membrane transporter molecules
 s_lm = 0.5e-6                    # um2, specific surface area of membrane lipid molecules
-Mmax = 0.00045                   # unitless, maximum membrane transporter to lipid ratio
-Mmin = 3e-5                      # unitless, minimum membrane transporter to lipid ratio
-Vp = 3e-5                        # um3, volume / photosystems
+Mmax = 1e-5                      # unitless, maximum membrane transporter to lipid ratio
+Mmin = 3e-6                      # unitless, minimum membrane transporter to lipid ratio
+Vp = 1.5e-5                      # um3, volume / photosystem
 Vli = 1.5e-9                     # um3, volume / lipid droplet
-Vnu = 50                         # nucleus volume, um3
+Vnu = 0.05                       # nucleus volume, um3
 Dmax = 270/1e15*(6.02e23)/9      # Da/um3, maximum density of the cell
-ctag_min = 200                   # minimum number of lipid molecules / um3
+ctag_min = 200                   # minimum number of lipid molecules / um3 (alpha_min)
 
 # Temperature effects
 Ttest = 273.0 .+ [3.0:1.0:45.0;] # K, temperature regime
@@ -30,7 +32,7 @@ Tmax = maximum(Ttest)            # K, maximal temperature
 Ed =  1.4                        # eV, deactivation energy
 R    = 8.62*10^-5                # eV/K, Boltzmann's constant
 Tref = 20+273.0                  # K, reference temperature
-Td   = 35+273.0 #43+273.0        # K, critical high temperature
+Td   = 35+273.0                  # K, critical high temperature
 
 # Molecular weights
 ηaa = 110                        # Da / amino acid
@@ -64,9 +66,14 @@ e_ru = 10                        # ATP / carbon
 e_tr = 1.0                       # ATP / nitrogen
 e_ri = 3+9*5                     # ATP / amino acid
 e_lb = 3.9                       # ATP / carbon
+#e_u  = 4.5#2e7/ctag_max#4.5              # ATP / photosystem
 e_re = 4.5                       # ATP / photosystem
 e_gl = 5.0                       # ATP / carbon
 e_ld = 6.6                       # ATP / carbon
 
 fdr = 0.25                       # fraction of respiration that happens in the dark
 td = 12*60                       # minutes, dark period (12 hours)
+
+# Set parameters that will be targeted in the sensitivity analyses
+change_Ea = 0.85                 # eV, thermal dependency of metabolic reactions
+change_ctag_max = 8e6            # maximum number of lipid molecules / um3, heat-mitigation capacity (alpha)
